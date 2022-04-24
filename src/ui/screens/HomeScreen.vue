baaddab81e605ad6daf315dcc09ed2026ee4658e
Bartosz Laskowski <laskowski.bartosz99@gmail.com>
2022-04-24 17:40:01 +0200

    Added button

--- src/ui/screens/HomeScreen.vue
+++ src/ui/screens/HomeScreen.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-    <router-link to="/signin">Sign In</router-link>
+    <router-link :to="{name: 'CreatePost'}">Type what you are thinking aboout…</router-link>
 </template>
 
 <style scoped lang="scss">
+    a {
+        background-color: $surfaceVariant;
+        color: rgba($onSurfaceVariant, 0.7);
+        border: none;
+        border-radius: 24px;
+        padding: 1rem;
+        text-decoration: none;
+        font-size: 0.8rem;
 
+        &:hover {
+            cursor: pointer;
+        }
+    }
 </style>
