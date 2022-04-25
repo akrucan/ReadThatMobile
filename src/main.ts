d6645ab893a71c88116e532ead6ce3030ac1622a
pwoicik <patrykwoicik@gmail.com>
2022-04-25 11:57:35 +0200

    new routes added, not implemented tho

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
-import { createPinia } from "pinia";
 import App from "./ui/App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: "/",
+            redirect: { name: "Home", replace: true },
+        },
+        {
+            path: "/home",
             name: "Home",
             component: () => import("./ui/screens/HomeScreen.vue"),
+        },
+        {
+            path: "/search",
+            name: "Search",
+            component: () => import("./ui/screens/SearchScreen.vue"),
+        },
+        {
+            path: "/profile",
+            name: "Profile",
+            component: () => import("./ui/screens/ProfileScreen.vue"),
         },
         {
             path: "/signin",
@@ -19,17 +33,14 @@ const router = createRouter({
         {
             path: "/signup",
             name: "SignUp",
-            component: () => import("./ui/screens/SignUpScreen.vue")
+            component: () => import("./ui/screens/SignUpScreen.vue"),
         },
-                {
+        {
             path: "/createpost",
             name: "CreatePost",
-            component: () => import("./ui/screens/CreatePostScreen.vue")
-        }
+            component: () => import("./ui/screens/CreatePostScreen.vue"),
+        },
     ],
 });
 
-createApp(App)
-    .use(router)
-    .use(createPinia())
-    .mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
